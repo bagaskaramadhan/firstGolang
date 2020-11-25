f828fa02bd1904cc92aadf1105df84092c7475ca
bagaskaramadhan <bagaskaramadhan@gmail.com>
2020-11-25 22:33:21 +0700

    datatype

---
+++ variable/datatype.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstname = "Bagaskara"
+	var lastname = "Ramadhan"
+	var age = 23
+	var isMarried = false
+
+	fmt.Println(" Name: ", firstname+" "+lastname+"\n", "Age: ", age, "\n", "Married: ", isMarried)
+}
